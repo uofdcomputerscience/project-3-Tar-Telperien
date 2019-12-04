c6308c035d5750e019a513ff0445e7c3101b2c09
Tar-Telperien <venicapiea@verizon.net>
2019-12-04 10:16:44 -0600

    Begun project

---
+++ Project3Sample/BookCell.swift
@@ -0,0 +1,13 @@
+//
+//  BookCell.swift
+//  Project3Sample
+//
+//  Created by Amelia on 03/12/2019.
+//  Copyright © 2019 Russell Mirabelli. All rights reserved.
+//
+
+import UIKit
+
+class BookCell: UITableViewCell {
+    
+}
--- Project3Sample/BookListViewController.swift
+++ Project3Sample/BookListViewController.swift
@@ -10,7 +10,23 @@ import UIKit
 
 class BookListViewController: UIViewController {
     
+    @IBOutlet weak var bookListView: UITableView!
     
+}
+
+extension BookListViewController: UITableViewDelegate {
+    
+}
+
+extension BookListViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        //stuff
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "BookCell", for: indexPath) as! BookCell
+        return cell
+    }
     
     
 }
